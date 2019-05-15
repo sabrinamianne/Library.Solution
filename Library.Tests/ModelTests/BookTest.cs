5fa4ad01150c105f03e137c09d96f739bab48248
Sabrina Mianne <sabrina.epicodus@gmail.com>
2019-05-15 10:41:00 -0700

    Create Library Project

---
+++ Library.Tests/ModelTests/BookTest.cs
@@ -0,0 +1,14 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Library.Models;
+using System.Collections.Generic;
+using System;
+
+namespace Library.TestTools
+{
+  [TestClass]
+  public class BookTest : IDisposable
+  {
+
+
+  }
+}
---
+++ Library/Models/Client.cs
@@ -0,0 +1,36 @@
+using System.Collections.Generic;
+using MySql.Data.MySqlClient;
+
+namespace Library.Models
+{
+  public class Client
+  {
+    private string _name;
+    private int _id;
+
+    public Client (string name, int id=0)
+    {
+      _name = name;
+      _id = id;
+    }
+
+    public string GetName()
+    {
+      return _name;
+    }
+
+    public void SetName (string newName)
+    {
+      _name = newName;
+    }
+
+    public int GetId()
+    {
+      return _id;
+    }
+
+    
+
+
+  }
+}
